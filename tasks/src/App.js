c7a095cae1baf01fe8b0f1d0c451435b791ea359
KeucaLV <ipa21.n.markitans@vtdt.edu.lv>
2023-11-07 21:07:46 +0200

    hz

--- tasks/src/App.js
+++ tasks/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Tasks from "./components/tasks";
 import MonthCalendar from "./components/calendar";
+import Calendar from "./components/zohoCalendar";
 function App() {
     return (
         <>
-        <Tasks  />
-        <MonthCalendar />
+        {/*<Tasks  />*/}
+        {/*<MonthCalendar />*/}
+            <Calendar />
         </>
     );
 }
--- tasks/src/components/calendar.js
+++ tasks/src/components/calendar.js
@@ -10,7 +10,6 @@ const MonthCalendar = () => {
     const tasks = [
         { date: 11, taskName: "Meeting", profilePic: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png", dueDate: "2023-11-05", priority: 3 },
         { date: 12, taskName: "Presentation", profilePic: "https://images.squarespace-cdn.com/content/v1/60d2052696041e771b8b7a60/6173a084-afec-4724-8b96-d2955e1844ec/%C2%A9MirjamLetsch-AUGURK-4.jpg", dueDate: "2023-11-12", priority: 4 },
-        // Add more tasks as needed
     ];
 
     const daysInMonth = Array.from({ length: lastDayOfMonth }, (_, i) => i + 1);
@@ -49,7 +48,7 @@ const MonthCalendar = () => {
                                                 <p>Priority: {task.priority}</p>
                                                 <div className="star-rating">
                                                     {[...Array(task.priority)].map((_, i) => (
-                                                        <span key={i} className="star">&#9733;</span>
+                                                        <span key={i} className="star">⭐</span>
                                                     ))}
                                                 </div>
                                             </div>
